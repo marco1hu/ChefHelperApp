1c55dcad6d47e3451b42c2deb8eea7141b392373
Marco Hu <humarco1310@gmail.com>
2024-09-01 02:35:57 +0200

    Initial Commit

---
+++ ChefHelperApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChefHelperApp
+//
+//  Created by IFTS40 on 01/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
