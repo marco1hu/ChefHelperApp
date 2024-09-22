0c8c445c2722b605de99a99b6749bb599d959765
Marco Hu <humarco1310@gmail.com>
2024-09-22 15:29:20 +0200

    Search

---
+++ ChefHelperApp/SearchResultsTableViewCell/SearchResultsTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  SearchResultsTableViewCell.swift
+//  ChefHelperApp
+//
+//  Created by IFTS40 on 22/09/24.
+//
+
+import UIKit
+
+class SearchResultsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
